c2276df0b8add1416c3242fd3fe20ca861c93015
madsnot <dashenka.proskuryakova@gmail.com>
2022-11-12 22:00:54 +0300

    Refactor project structure

--- main.go
+++ main.go
@@ -1,37 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"example/billing-test/config"
+	"example/billing-test/routers_handlers"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-
-	"database/sql"
-
-	_ "github.com/lib/pq"
-
-	"strconv"
-
-	"example/billing-test/config"
-
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
-
-type UserBalance struct {
-	ID              int `json:"id"`
-	UserID          int `json:"userId"`
-	Balance         int `json:"balance"`
-	ReservedBalance int `json:"reserve"`
-}
-
-type Transaction struct {
-	ID        int    `json:"id"`
-	Type      string `json:"type"`
-	UserID    int    `json:"userId"`
-	OrderID   int    `json:"orderId"`
-	ServiceID int    `json:"serviceId"`
-	Amount    int    `json:"amount"`
-}
 
 func ValidOpenDataBase(function func(*gin.Context, *sql.DB)) gin.HandlerFunc {
 
@@ -54,65 +32,6 @@ func ValidOpenDataBase(function func(*gin.Context, *sql.DB)) gin.HandlerFunc {
 	}
 }
 
-func TopUpUserBalance(context *gin.Context, dataBase *sql.DB) {
-	var userBalance UserBalance
-	balance := 0
-
-	if errBindJSON := context.BindJSON(&userBalance); errBindJSON != nil {
-		log.Fatal("errBindJSON: ", errBindJSON)
-		return
-	}
-
-	rowsUserBalance := dataBase.QueryRow("select user_balance.balance from user_balance "+
-		"where user_balance.user_id = $1", userBalance.UserID)
-
-	if errRowScan := rowsUserBalance.Scan(&balance); errRowScan != nil {
-		_, errInsert := dataBase.Exec("insert into user_balance (user_id, balance) values ($1, $2)",
-			userBalance.UserID, userBalance.Balance)
-		if errInsert != nil {
-			log.Fatal("errInsert: ", errInsert)
-			return
-		}
-	} else {
-		userBalance.Balance += balance
-		_, errUpdate := dataBase.Exec("update user_balance set balance = $1 where user_id = $2",
-			userBalance.Balance, userBalance.UserID)
-		if errUpdate != nil {
-			log.Fatal("errUpdate: ", errUpdate)
-			return
-		}
-	}
-	dataBase.Close()
-	context.JSON(http.StatusOK, "Balance has been replenished")
-}
-
-func GetUserBalance(context *gin.Context, dataBase *sql.DB) {
-	var userBalance UserBalance
-
-	userBalance.UserID, _ = strconv.Atoi(context.Param("userId"))
-
-	rowsUserBalance := dataBase.QueryRow("select user_balance.balance from user_balance "+
-		"where user_balance.user_id = $1", userBalance.UserID)
-
-	if errRowScan := rowsUserBalance.Scan(&userBalance.Balance); errRowScan != nil {
-		context.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
-		return
-	}
-	dataBase.Close()
-	context.JSON(http.StatusOK, userBalance.Balance)
-}
-
-func ReserveAmountForPayment(context *gin.Context, dataBase *sql.DB) {
-
-	//if UserBalance.Balance, err = (получение из базы в UserBalance по userID); UserBalance.Balance == 0 || err != nil
-	// {context.JSON(http.StatusBadRequest, gin.H{"message:", "Account has insufficient funds"}) return}
-
-}
-
-func ReserveWriteOff(context *gin.Context) {
-
-}
-
 func init() {
 	if errLoadEnv := godotenv.Load(); errLoadEnv != nil {
 		log.Print("File .env not found")
@@ -122,10 +41,10 @@ func init() {
 func main() {
 	router := gin.Default()
 
-	router.POST("/balance/topUp", ValidOpenDataBase(TopUpUserBalance))
-	router.GET("/balance/:userId", ValidOpenDataBase(GetUserBalance))
-	router.POST("/payment/reserve", ValidOpenDataBase(ReserveAmountForPayment))
-	router.POST("/payment", ReserveWriteOff)
+	router.POST("/balance/topUp", ValidOpenDataBase(routers_handlers.TopUpUserBalance))
+	router.GET("/balance/:userId", ValidOpenDataBase(routers_handlers.GetUserBalance))
+	router.POST("/payment/reserve", ValidOpenDataBase(routers_handlers.ReserveAmountForPayment))
+	router.POST("/payment", ValidOpenDataBase(routers_handlers.ReserveWriteOff))
 
 	router.Run("localhost:8080")
 }
