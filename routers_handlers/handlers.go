805f0807c6d5d62bfc1112fa7956a63676cce372
madsnot <dashenka.proskuryakova@gmail.com>
2022-11-12 23:33:17 +0300

    Add reserve amount for payment and add make transaction to top up

--- routers_handlers/handlers.go
+++ routers_handlers/handlers.go
@@ -28,32 +28,42 @@ type Transaction struct {
 
 func TopUpUserBalance(context *gin.Context, dataBase *sql.DB) {
 	var userBalance UserBalance
-	balance := 0
+	var transaction Transaction
+	userBalance.Balance = 0
 
-	if errBindJSON := context.BindJSON(&userBalance); errBindJSON != nil {
+	if errBindJSON := context.BindJSON(&transaction); errBindJSON != nil {
 		log.Fatal("errBindJSON: ", errBindJSON)
 		return
 	}
 
 	rowsUserBalance := dataBase.QueryRow("select user_balance.balance from user_balance "+
-		"where user_balance.user_id = $1", userBalance.UserID)
+		"where user_balance.user_id = $1", transaction.UserID)
 
-	if errRowScan := rowsUserBalance.Scan(&balance); errRowScan != nil {
+	if errRowScan := rowsUserBalance.Scan(&userBalance.Balance); errRowScan != nil {
 		_, errInsert := dataBase.Exec("insert into user_balance (user_id, balance) values ($1, $2)",
-			userBalance.UserID, userBalance.Balance)
+			transaction.UserID, transaction.Amount)
 		if errInsert != nil {
 			log.Fatal("errInsert: ", errInsert)
 			return
 		}
 	} else {
-		userBalance.Balance += balance
+		userBalance.Balance += transaction.Amount
 		_, errUpdate := dataBase.Exec("update user_balance set balance = $1 where user_id = $2",
-			userBalance.Balance, userBalance.UserID)
+			userBalance.Balance, transaction.UserID)
 		if errUpdate != nil {
 			log.Fatal("errUpdate: ", errUpdate)
 			return
 		}
 	}
+
+	transaction.Type = "top_up"
+	_, errMakeTopUpTransaction := dataBase.Exec("insert into transaction (type, user_id, amount)"+
+		"values ($1, $2, $3)", transaction.Type, transaction.UserID, transaction.Amount)
+	if errMakeTopUpTransaction != nil {
+		log.Fatal("errMakeTopUpTransaction: ", errMakeTopUpTransaction)
+		return
+	}
+
 	dataBase.Close()
 	context.JSON(http.StatusOK, "Balance has been replenished")
 }
@@ -75,10 +85,45 @@ func GetUserBalance(context *gin.Context, dataBase *sql.DB) {
 }
 
 func ReserveAmountForPayment(context *gin.Context, dataBase *sql.DB) {
+	var userBalance UserBalance
+	var transaction Transaction
 
-	//if UserBalance.Balance, err = (получение из базы в UserBalance по userID); UserBalance.Balance == 0 || err != nil
-	// {context.JSON(http.StatusBadRequest, gin.H{"message:", "Account has insufficient funds"}) return}
+	if errBindJSON := context.BindJSON(&transaction); errBindJSON != nil {
+		log.Fatal("errBindJSON: ", errBindJSON)
+		return
+	}
 
+	rowsUserBalance := dataBase.QueryRow("select user_balance.balance from user_balance "+
+		"where user_balance.user_id = $1", transaction.UserID)
+
+	if errRowScan := rowsUserBalance.Scan(&userBalance.Balance); errRowScan != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+
+	if userBalance.Balance < transaction.Amount {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Account has insufficient funds"})
+		return
+	}
+	userBalance.Balance -= transaction.Amount
+
+	_, errReserveAmount := dataBase.Exec("update user_balance set balance = $1, reserved_balance = $2 where user_id = $3",
+		userBalance.Balance, transaction.Amount, transaction.UserID)
+	if errReserveAmount != nil {
+		log.Fatal("errReserveAmount: ", errReserveAmount)
+		return
+	}
+
+	transaction.Type = "reserve"
+	_, errMakeReserveTransaction := dataBase.Exec("insert into transaction (type, user_id, order_id, service_id, amount)"+
+		"values ($1, $2, $3, $4, $5)", transaction.Type, transaction.UserID, transaction.OrderID, transaction.ServiceID,
+		transaction.Amount)
+	if errMakeReserveTransaction != nil {
+		log.Fatal("errMakeReserveTransaction: ", errMakeReserveTransaction)
+		return
+	}
+	dataBase.Close()
+	context.JSON(http.StatusOK, gin.H{"message": "Payment successful"})
 }
 
 func ReserveWriteOff(context *gin.Context, dataBase *sql.DB) {
